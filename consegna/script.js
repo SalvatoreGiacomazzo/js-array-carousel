b7610c27a97c35e4d7ea9481752efd5cf4d26975
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-08-22 18:25:38 +0200

    exercise finished with no bonus

--- consegna/script.js
+++ consegna/script.js
@@ -13,14 +13,30 @@
  const leftButton = document.getElementById('left');
  const rightButton = document.getElementById('right');
  const gallery = document.getElementById('gallery')
- const carouselPics = document.querySelectorAll('#gallery img')
-
-
+ const carouselPics = document.querySelectorAll('.item')
 //Creazione della variabile che gestisce il cambio delle foto
 let visibilityIndex= 0;
  
 //Rendo visibile la prima immagine
 carouselPics[visibilityIndex].classList.add('visible')
+
+console.log(carouselPics)
+
+//Eventi della freccia destra
+rightButton.addEventListener('click', function (){
+carouselPics[visibilityIndex].classList.remove('visible') //Rimuovo la classe della visibilità a qualunque elemento la abbia
+visibilityIndex++; //Aumento l'indice di visibilità per rendere visibile la foto successiva
+carouselPics[visibilityIndex].classList.add('visible')
+})
+
+
+//Eventi della freccia sinsitra
+leftButton.addEventListener('click', function (){
+   carouselPics[visibilityIndex].classList.remove('visible') //Rimuovo la classe della visibilità a qualunque elemento la abbia
+   visibilityIndex--; //Decremento l'indice di visibilità per rendere visibile la foto successiva
+   carouselPics[visibilityIndex].classList.add('visible')
+   })
+   
 
 
     
